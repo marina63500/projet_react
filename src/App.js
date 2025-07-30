399668094e6aebe966f59e57b5eda2a94660f317
Aloula96 <aloula.afewerki@gmail.com>
2025-07-30 14:19:44 +0200

    WIP: local changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Famille from './pages/Famille';
-
+import Famille from "./pages/Famille";
 
 const App = () => {
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/> 
-      <Route path="/famille" element={<Famille />}/> 
-      {/* Add more routes as needed */}     
-      {/* path = "*" fonctionne qd l'url ne correspond à rien de déclarer au dessus,il fait comme une redirection */}
-      <Route path="*" element={<Home />}/>
-    </Routes>
-  
-  </BrowserRouter>
- 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/famille" element={<Famille />} />
+        <Route path="/messagerie" element={<Messagerie />} />
+        {/* Add more routes as needed */}
+        {/* path = "*" fonctionne qd l'url ne correspond à rien de déclarer au dessus,il fait comme une redirection */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
